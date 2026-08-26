108f087ad60021597bb519b09eb03a4face82acf
Praveen Sonesha <praveensonesha2003@gmail.com>
2026-08-26 10:47:53 +0530

    Update worker.js

--- api/worker.js
+++ api/worker.js
@@ -21,7 +21,11 @@
 const ALLOWED_ORIGINS = new Set([
   "https://bitroot.org",
   "https://www.bitroot.org",
+  "https://stag-bitroot-org.bitroot.in",
   "http://localhost:3000",
+  "http://localhost:3001",
+  "http://localhost:3002",
+  "http://localhost:3003",
   "http://localhost:8000",
 ]);
 
